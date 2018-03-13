6aa3151a5e6d839ba46177b2cd6b9768b24bfda7
758770768 <758770768@qq.com>
2018-03-13 12:35:47 +0800

    aaa

--- src/hello/Hello3.java
+++ src/hello/Hello3.java
@@ -5,7 +5,7 @@ public class Hello3 {
 	public static void main(String[] args) {
 		System.out.println("hell03");
 		System.out.println("hello4");
-		System.out.println("i am a");
+		System.out.println("i am ahahhahha");
 	}
 
 }
