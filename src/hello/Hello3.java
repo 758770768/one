52f8aa8ad1e9649cdf6f979ab822783367feab1e
758770768 <758770768@qq.com>
2018-03-13 12:58:24 +0800

    po

--- src/hello/Hello3.java
+++ src/hello/Hello3.java
@@ -3,12 +3,7 @@ package hello;
 public class Hello3 {
 
 	public static void main(String[] args) {
-		System.out.println("hell03");
-		System.out.println("hello4");
-		System.out.println("i am ahahhahha");
-		System.out.println("this is a beautiful day");
-		System.out.println("i am b");
-		System.out.println("i amd very b");
+	 
 		System.out.println("12345");
 	}
 
